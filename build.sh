afa7052a8527a1e3e3291555866795014ea8a507
George Vella <mail@georgevella.com>
2020-10-06 17:25:52 +0200

    Moved sln to upper folder

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build . -f ./src/Shipbot.Controller/Dockerfile -t georgevella/shipbot:dev
--- src/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build . -f ./Shipbot.Controller/Dockerfile -t georgevella/shipbot:dev
