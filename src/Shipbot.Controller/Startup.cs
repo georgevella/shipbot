ed3ce78d4a9cdace1904823bb25c15e6befcb8c8
George Vella <mail@georgevella.com>
2020-10-09 12:45:21 +0200

    swagger support

--- src/Shipbot.Controller/Startup.cs
+++ src/Shipbot.Controller/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Converters;
 using Quartz;
 using Quartz.Impl;
@@ -61,6 +62,12 @@ namespace Shipbot.Controller
 
             services.AddHealthChecks();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Shipbot API", Version = "v1"});
+            });
+
+            services.AddSwaggerGenNewtonsoftSupport();
             services.Configure<ShipbotConfiguration>(Configuration.GetSection("Shipbot"));
             services.Configure<SlackConfiguration>(Configuration.GetSection("Slack"));
 
@@ -110,6 +117,12 @@ namespace Shipbot.Controller
             }
 
             app.UseHealthChecks("/health");
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shipbot API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseRouting();
