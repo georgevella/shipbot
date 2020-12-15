1f1a9733e63196cc839686c1c35593bdc05ec302
George Vella <mail@georgevella.com>
2020-12-15 11:36:30 +0100

    Fixed issue with IOC

--- src/Shipbot.Data/Internal/ShipbotDatabaseMigrationHostedService.cs
+++ src/Shipbot.Data/Internal/ShipbotDatabaseMigrationHostedService.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -9,20 +11,25 @@ namespace Shipbot.Data.Internal
     public class ShipbotDatabaseMigrationHostedService : IHostedService
     {
         private readonly ILogger<ShipbotDatabaseMigrationHostedService> _log;
-        private readonly ShipbotDbContext _dbContext;
+        private readonly IServiceProvider _serviceProvider;
 
         public ShipbotDatabaseMigrationHostedService(
             ILogger<ShipbotDatabaseMigrationHostedService> log,
-            ShipbotDbContext dbContext)
+            IServiceProvider serviceProvider
+            )
         {
             _log = log;
-            _dbContext = dbContext;
+            _serviceProvider = serviceProvider;
         }
         
         public async Task StartAsync(CancellationToken cancellationToken)
         {
+            using var scope = _serviceProvider.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetService<ShipbotDbContext>();
+            
             _log.LogInformation("Checking database ...");
-            await _dbContext.Database.MigrateAsync(cancellationToken: cancellationToken);
+            await dbContext.Database.MigrateAsync(cancellationToken: cancellationToken);
             _log.LogInformation("Checking database ... done");
         }
 
