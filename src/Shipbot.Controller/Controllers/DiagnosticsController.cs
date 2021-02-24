5e3e52bb863bf406c1f5d90d2ecbbac570ec2904
George Vella <mail@georgevella.com>
2021-02-24 18:26:56 +0100

    Improved local image cache query facility

--- src/Shipbot.Controller/Controllers/DiagnosticsController.cs
+++ src/Shipbot.Controller/Controllers/DiagnosticsController.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using k8s;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Shipbot.Applications;
+using Shipbot.Applications.Models;
 using Shipbot.ContainerRegistry;
 using Shipbot.ContainerRegistry.Models;
 using Shipbot.ContainerRegistry.Services;
+using Shipbot.Controller.Core.Configuration.Apps;
 using Shipbot.Controller.DTOs;
 using Shipbot.Deployments;
 using Shipbot.SlackIntegration;
@@ -51,20 +56,66 @@ namespace Shipbot.Controller.Controllers
 
         [HttpGet("container-image-local-store")]
         [Authorize]
-        public async Task<ActionResult> GetContainerImageIntoLocalStore([FromQuery] string repository)
+        public async Task<ActionResult> GetContainerImageIntoLocalStore([FromQuery] string repository, [FromQuery] string pattern)
         {
+            if (string.IsNullOrEmpty(repository))
+                return BadRequest();
+            
             var containerImages = await _containerImageMetadataService.GetTagsForRepository(repository);
 
-            return Ok(containerImages
-                .Select(x => new ContainerImageDto()
+            if (string.IsNullOrWhiteSpace(pattern))
+            {
+                return Ok(new
                 {
-                    Hash = x.Hash,
-                    Repository = x.Repository,
-                    Tag = x.Tag,
-                    CreationDateTime = x.CreationDateTime
-                })
-                .ToList()
-            );
+                    Items = containerImages.Select(x => (ContainerImageDto) x).ToList()
+                });
+            }
+
+            var parts = pattern.Split(':');
+
+            if (parts.Length == 0)
+                throw new InvalidOperationException("wtf?");
+
+            var actualPattern = pattern;
+
+            if (!Enum.TryParse<UpdatePolicy>(parts[0], ignoreCase: true, out var policyType))
+            {
+                // first part is not a policy type, handle later
+
+            }
+            else
+            {
+                // since above we split the pattern with ':', let's assume that the type is part[0],
+                // while the pattern is part[1..end].  If there are more than two parts, then the pattern contained
+                // ':', thus we need to rebuild the pattern as is by joining all the parts together.
+                actualPattern = string.Join(":", parts.Skip(1));
+            }
+
+            var policy = policyType switch
+            {
+                UpdatePolicy.Glob => (ImageUpdatePolicy) new GlobImageUpdatePolicy(actualPattern),
+                UpdatePolicy.Regex => new RegexImageUpdatePolicy(actualPattern),
+                UpdatePolicy.Semver => new SemverImageUpdatePolicy(actualPattern),
+                _ => null
+            };
+
+            if (policy == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(new
+            {
+                policy = policy,
+                Items = containerImages
+                    .Where(x => policy.IsMatch(x.Tag))
+                    .OrderByDescending(x => x.CreationDateTime)
+                    .Select(x => (ContainerImageDto) x)
+                    .ToList()
+            });
+
+
+
         }
 
         [HttpPost("container-image-local-store")]
