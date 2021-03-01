2c1f1d9fed6096214f6016125d90b4c24b5fa620
George Vella <mail@georgevella.com>
2021-03-01 18:27:37 +0100

    Yaml utilities support array and map accessors

--- src/Shipbot.DeploymentSources/YamlUtilities.cs
+++ src/Shipbot.DeploymentSources/YamlUtilities.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
 using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
 
@@ -42,10 +48,27 @@ namespace Shipbot.Controller.Core.ApplicationSources
             
             writer.Flush();
         }
+        
+        const string AccessorPattern = @"(?<name>[\w\d\s]+)\[(?<index>[\w\d\""\']+)\]";
 
-        public string ExtractValueFromDoc(string tagPropertyPath, YamlDocument doc)
+        private YamlScalarNode? VisitDocAndGetValueNode(string path, YamlDocument doc)
         {
-            var parts = tagPropertyPath.Split('.');
+            YamlMappingNode? GetMappingNodeFromArray(YamlSequenceNode sequenceNode, int index)
+            {
+                if (sequenceNode.Children.Count <= index)
+                    return null;
+                
+                return sequenceNode.Children[index] as YamlMappingNode 
+                       ?? throw new InvalidOperationException("Retrieved child from array is NOT a map type."); 
+            }
+                        
+            YamlMappingNode? GetMappingNodeFromMap(YamlMappingNode mappingNode, string property)
+            {
+                return mappingNode.Children[property] as YamlMappingNode 
+                       ?? throw new InvalidOperationException("Retrieved child from map is NOT a map type."); 
+            }
+
+            var parts = path.Split('.');
 
             var node = doc.RootNode as YamlMappingNode;
 
@@ -54,51 +77,103 @@ namespace Shipbot.Controller.Core.ApplicationSources
                 if (node == null)
                     break;
 
-                if (!node.Children.ContainsKey(part)) 
+                string? mapAccessorKey = null;
+                int? arrayAccessorIndex = null;
+                string propertyName = part;
+
+                // check if part is an array accessor
+                var matches = Regex.Match(part, AccessorPattern);
+                if (matches.Success)
+                {
+                    // we have an array accessor
+                    propertyName = matches.Groups["name"].Value;
+                    var rawAccessorIndex = matches.Groups["index"].Value;
+
+                    if (int.TryParse(rawAccessorIndex, out var aai))
+                    {
+                        // index is an integer, assume we are going to read an array from property
+                        arrayAccessorIndex = aai;
+                    }
+                    else
+                    {
+                        mapAccessorKey = rawAccessorIndex.Trim().Trim('\"', '\'');
+                    }
+                }
+
+                if (!node.Children.ContainsKey(propertyName)) 
                     break;
                 
                 if (parts.Last().Equals(part))
                 {
-                    if (node.Children[part] is YamlScalarNode valueNode)
+                    // we reached the end of the provided path, return the value we found
+                    if (arrayAccessorIndex == null && mapAccessorKey == null)
                     {
-                        return valueNode.Value;
+                        // we are not trying to access a map or array
+                        if (node.Children[propertyName] is YamlScalarNode valueNode)
+                        {
+                            return valueNode;
+                        }
+                    }
+                    else
+                    {
+                        // we are reading a single value from within an array or a map
+                        var childNode = node.Children[propertyName];
+                        var valueNode = childNode switch
+                        {
+                            YamlSequenceNode arrayNode => arrayAccessorIndex != null
+                                ? (YamlScalarNode)arrayNode.Children[arrayAccessorIndex.Value]
+                                : throw new InvalidOperationException(
+                                    "We are trying to access an array node, but index value is not an integer."),
+                            YamlMappingNode mappingNode => mapAccessorKey != null
+                                ? (YamlScalarNode)mappingNode.Children[mapAccessorKey]
+                                : throw new InvalidOperationException(
+                                    "We are trying to access a map node, but index value is not a string."),
+                            _ => throw new InvalidOperationException("Found mismatching type")
+                        };
+                        
+                        return valueNode;
                     }
                 }
                 else
                 {
-                    node = node.Children[part] as YamlMappingNode;
+                    if (arrayAccessorIndex == null && mapAccessorKey == null)
+                    {
+                        node = node.Children[propertyName] as YamlMappingNode;
+                    }
+                    else
+                    {
+                        var childNode = node.Children[propertyName];
+                        node = childNode switch
+                        {
+                            YamlSequenceNode arrayNode => arrayAccessorIndex != null
+                                ? GetMappingNodeFromArray(arrayNode, arrayAccessorIndex.Value)
+                                : throw new InvalidOperationException(
+                                    "We are trying to access an array node, but index value is not an integer."),
+                            YamlMappingNode mappingNode => mapAccessorKey != null
+                                ? GetMappingNodeFromMap(mappingNode, mapAccessorKey)
+                                : throw new InvalidOperationException(
+                                    "We are trying to access a map node, but index value is not a string."),
+                            _ => throw new InvalidOperationException("Found mismatching type")
+                        };
+                    }
+                        
                 }
             }
             
             return null;
         }
+
+        public string? ExtractValueFromDoc(string tagPropertyPath, YamlDocument doc)
+        {
+            var valueNode = VisitDocAndGetValueNode(tagPropertyPath, doc);
+            return valueNode?.Value;
+        }
         
         public void SetValueInDoc(string tagPropertyPath, YamlDocument doc, string newValue)
         {
-            var parts = tagPropertyPath.Split('.');
-
-            var node = doc.RootNode as YamlMappingNode;
-
-            foreach (var part in parts)
-            {
-                if (node == null)
-                    break;
-
-                if (!node.Children.ContainsKey(part)) 
-                    break;
-                
-                if (parts.Last().Equals(part))
-                {
-                    if (node.Children[part] is YamlScalarNode valueNode)
-                    {
-                        valueNode.Value = newValue;
-                    }
-                }
-                else
-                {
-                    node = node.Children[part] as YamlMappingNode;
-                }
-            }
+            var valueNode = VisitDocAndGetValueNode(tagPropertyPath, doc);
+            if (valueNode != null)
+                valueNode.Value = newValue;
         }
     }
 }
--- src/Shipbot.Tests/YamlFileTests.cs
+++ src/Shipbot.Tests/YamlFileTests.cs
@@ -54,6 +54,62 @@ sheep:
 
             text.Should().Be(expected);
 
+        }  
+        
+        [Theory]
+        [InlineData("instances[0].image.tag", "develop-2892")]
+        [InlineData("instances[2].field", "xyz")]
+        [InlineData("instances[4].field", null)]
+        public void GetValue_SingleDocument_ArrayPath(string path, string expected)
+        {
+            var x = @"
+instances:
+- field: abc
+  image:
+    tag: develop-2892
+- field: def
+  image:
+    tag: develop-123
+- field: xyz
+  image:
+    tag: develop-10";
+
+            var byteArray = Encoding.UTF8.GetBytes(x);
+            var inStream = new MemoryStream(byteArray);
+
+            var yamlUtilities = new YamlUtilities();
+            var yaml = new YamlStream();
+            yamlUtilities.ReadYamlStream(yaml, inStream);
+
+            var doc = yaml.Documents.First();
+            var readValue = yamlUtilities.ExtractValueFromDoc(path, doc);
+
+            readValue.Should().Be(expected);
+        }
+        
+        [Theory]
+        [InlineData("cat[\"image1\"].tag", "develop-2892")]
+        [InlineData("cat.image1[tag]", "develop-2892")]
+        public void GetValue_SingleDocument_MapPath(string path, string expected)
+        {
+            var x = @"
+cat:
+  image1:
+    tag: develop-2892
+  image2:
+    tag: develop-123";
+
+            var byteArray = Encoding.UTF8.GetBytes(x);
+            var inStream = new MemoryStream(byteArray);
+
+            var yamlUtilities = new YamlUtilities();
+            var yaml = new YamlStream();
+            yamlUtilities.ReadYamlStream(yaml, inStream);
+
+            var doc = yaml.Documents.First();
+            var readValue = yamlUtilities.ExtractValueFromDoc(path, doc);
+
+            readValue.Should().Be(expected);
         }
         
         [Fact]
